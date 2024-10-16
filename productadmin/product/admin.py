23f17d323a589ccfb87e29853cb4172252cd3825
tranhuynhkyanh <thkyanhlxag@gmail.com>
2024-10-16 11:48:51 +0700

    delete

--- productadmin/product/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from productadmin.product.models import Product
-
-
-# Register your models here.
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'likes')
